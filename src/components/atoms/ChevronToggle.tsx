e1c4f747f7c998d86bbf7cb41fac10f5037d83e7
Torstein Thune <torstein@thune.io>
2023-07-28 10:44:34 +0200

    fix(components): ChevronToggle should no longer flex on mobile

--- src/components/atoms/ChevronToggle.tsx
+++ src/components/atoms/ChevronToggle.tsx
@@ -12,11 +12,13 @@ export default function ChevronToggle({
   ...rest
 }: ChevronToggleProps) {
   const innerClasses =
-    "items-center justify-center w-6 h-6 no-flex min-w-6 min-h-6 rounded-full border border-gray-200 print:hidden";
+    "flex items-center justify-center w-6 h-6 rounded-full border border-gray-200 print:hidden";
   return (
-    <FlexCol className={`${innerClasses} ${className}`} {...rest}>
-      {toggle && <ChevronDownIcon className="w-4 h-4" />}
-      {!toggle && <ChevronRightIcon className="w-4 h-4" />}
-    </FlexCol>
+    <div className="flex no-flex">
+      <FlexCol className={`${innerClasses} ${className}`} {...rest}>
+        {toggle && <ChevronDownIcon className="w-4 h-4" />}
+        {!toggle && <ChevronRightIcon className="w-4 h-4" />}
+      </FlexCol>
+    </div>
   );
 }
