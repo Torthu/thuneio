b7ac30395d03ddec03b0b4084db3769d69a5e80d
Torstein Thune <torstein@thune.io>
2023-09-11 16:29:53 +0200

    fix(components): add SubMenu.tsx

---
+++ src/components/atoms/SubMenu.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useCallback } from "react";
+import BaseComponent, { BaseComponentProps } from "./BaseComponent";
+
+interface SubmenuProps extends BaseComponentProps {
+  ToggleButton: React.ComponentType<BaseComponentProps>;
+}
+
+/** SubMenu
+ * Simple component for displaying a "pop-up" menu.
+ *
+ */
+export default function SubMenu({
+  as = "div",
+  className = "",
+  ToggleButton,
+  children,
+  ...rest
+}: SubmenuProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = useCallback(
+    () => setIsOpen(!isOpen),
+    [isOpen, setIsOpen]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener("click", handleClick);
+    }
+    return () => window.removeEventListener("click", handleClick);
+  }, [handleClick, isOpen]);
+
+  return (
+    <BaseComponent
+      as={as}
+      className={`relative ${className}`}
+      onClick={(e) => e.stopPropagation()}
+      {...rest}
+    >
+      <ToggleButton onClick={() => setIsOpen(!isOpen)} />
+
+      {isOpen && <div className="z-2 absolute right-0">{children}</div>}
+    </BaseComponent>
+  );
+}
