1fdf10f7b7446d0a70d363f4aa06286c10d3218c
Torstein Thune <torstein@thune.io>
2025-09-30 14:38:14 +0200

    add H3 component

---
+++ src/components/atoms/H3.tsx
@@ -0,0 +1,21 @@
+import BaseComponent, { BaseComponentProps } from "./BaseComponent";
+
+/** H2
+ * subheader - text-xl font-extralight
+ *
+ * @param as string | ReactComponent (default: "h1") Which DOM/React component to render as
+ *
+ * @example
+ *   <H2 as="h1">h1 with h2 styling</H1>
+ *
+ * @returns ReactNode
+ */
+export default function H3({
+  className = "",
+  as = "h3",
+  ...rest
+}: BaseComponentProps) {
+  className = `text-lg font-extralight text-slate-100 print:font-normal print:text-md ${className}`;
+
+  return <BaseComponent {...rest} className={className} as={as} />;
+}
